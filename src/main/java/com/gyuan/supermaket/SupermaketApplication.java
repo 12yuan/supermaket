0ed7b049a2bedd1f20096a76685edb57a8245825
Gongneng Yuan (EXT-NSB) <gongneng.yuan.ext@nokia-sbell.com>
2025-08-21 16:23:09 +0800

    初始化Spring Boot 3.5.4超市后端项目

---
+++ src/main/java/com/gyuan/supermaket/SupermaketApplication.java
@@ -0,0 +1,13 @@
+package com.gyuan.supermaket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermaketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermaketApplication.class, args);
+	}
+
+}
